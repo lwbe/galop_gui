37f5b5ec23b24acdccb4b6e296c9d62437a0bf8a
Lorenzo Bernardi <lorenzo.bernardi@llr.in2p3.fr>
2021-10-26 18:30:28 +0200

    finished Mock up viewer

--- galop.py
+++ galop.py
@@ -24,10 +24,9 @@ cmdmod /opt/pyrame/cmd_motion.xml &
 cmdmod /opt/pyrame/cmd_paths.xml &
 cmdmod /opt/pyrame/cmd_ls_460.xml &
 """
+_Nx, _Xi, _Xf, _Ny, _Yi, _Yf, _Nz, _Zi, _Zf = 10, -1, 1, 10, 0, 2, 10, -3, -1
 
-
-
-
+import json
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
@@ -176,10 +175,6 @@ class Pyrame(object):
 
         QApplication.restoreOverrideCursor()
         return retcode, res
-    
-
-    
-
 
 # thread for scan
 class MUWorker(QObject):
@@ -194,31 +189,37 @@ class MUWorker(QObject):
         self.move_params = move_params
         self._issuspended = False
 
-        self.lx = 30
-        self.ly = 30
-        self.points = np.arange(self.lx*self.ly)
-
-
     def run(self):
-
-
-
-
-        for i in self.points:
-            if not self._isrunning:
-                self.finished.emit()
-            elif not self._issuspended:
-                #QApplication.processEvents() # to get event from the GUI
-                y = int(i/self.lx)
-                x = i - y * self.lx
-                position = "%d,%d,%d" % (x,y,0.1)
-                bx = -np.sin(.10 * ((x/10.)**2 + (y/10)**2)) / 10.
-                field = "%f,%f,%f,%f" % (bx,-bx,2*bx,1-bx)
-                print("data", position, field)
+        lx, ly, lz = _Nx, _Ny, _Nz
+        nb_points = lx*ly*lz
+        xi, xf = _Xi, _Xf
+        yi, yf = _Yi, _Yf
+        zi, zf = _Zi, _Zf
+        i = 0
+        while self._isrunning:
+            if not self._issuspended:
+                i_z = int(i/(1.*ly*lx))
+                i_y = int((i-i_z*lx*ly)/(1.*lx))
+                i_x = int((i-i_z*lx*ly-i_y*lx))
+                x = xi+(xf-xi)/(lx-1.)*float(i_x)
+                y = yi+(yf-yi)/(ly-1.)*float(i_y)
+                z = zi+(zf-zi)/(lz-1.)*float(i_z)
+                #bx = -np.sin((x-z) ** 2 + y ** 2) / 10.
+                #by = (16. * x * (1-x) * y * (1-y) * np.sin(9 * np.pi * x) * np.sin(9 * np.pi * y))**2
+                #bz = x*np.exp(-((x-z)**2+y**2))
+                bx = np.sin(np.pi * x) * np.cos(np.pi * y) * np.cos(np.pi * z)
+                by = -np.cos(np.pi * x) * np.sin(np.pi * y) * np.cos(np.pi * z)
+                bz = (np.sqrt(2.0 / 3.0) * np.cos(np.pi * x) * np.cos(np.pi * y) * np.sin(np.pi * z))
+                bn = np.sqrt(bx*bx + by*by + bz*bz)
+                position = "%f,%f,%f" % (x, y ,z)
+                field = "%f,%f,%f,%f" % (bx, by, bz , bn
+                                         )
+                print("In worker ", x,y,z,i,i_x+i_y*lx+i_z*lx*lz)
                 self.field.emit("%s;%s" % (position, field))
-
-                self.progress.emit(100.*i/self.points[-1])
-
+                self.progress.emit(100.*i/(nb_points-1))
+                i += 1
+                if i == nb_points:
+                    self._isrunning = False
                 time.sleep(0.1)
 
         self.finished.emit()
@@ -337,24 +338,19 @@ class Scan3dPlotDialog(QDialog, Ui_Dialog):
         self.scan3d_update.setChecked(True)
         self.scan3d_plane.addItems(["xy", "yz", "xz"])
         self.scan3d_fieldcomponent.addItems(["Bx","By","Bz","B norm"])
-        self.scan3d_plottype.addItems(["surface","wireframe"])
+#        self.scan3d_plottype.addItems(["surface","surface and contour","wireframe"])
+        self.scan3d_plottype.addItems(["surface", "wireframe", "quiver"])
 
         self.scan3d_plottype.currentTextChanged.connect(self.update_plot_base)
-        self.scan3d_plottype.currentTextChanged.connect(self.update_plot_data_structure)
-
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update_plot)
-        self.is_started = False
+        self.scan3d_plane.currentTextChanged.connect(self.update_layers)
+        self.scan3d_layer.currentTextChanged.connect(self.update_plot_params)
+        self.scan3d_fieldcomponent.currentTextChanged.connect(self.update_field)
 
         #self.fig.set_canvas(self.canvas)
         self._ax = self.canvas.figure.add_subplot(projection="3d")
-
-        self._ax.set_xlabel("X")
-        self._ax.set_ylabel("Y")
-        self._ax.set_zlabel("Z")
-        #self.plot_wire()
         self._ax.view_init(30, 30)
-        self.plot_object=None
+        self.plot_object = None
+
 
     def stop(self):
         self.working_thread.stop()
@@ -362,30 +358,67 @@ class Scan3dPlotDialog(QDialog, Ui_Dialog):
 
     def suspend(self):
         self.working_thread.suspend()
-        if self.scan3d_suspend.text() == "suspend":
-            self.scan3d_suspend.setText("resume")
+        if self.scan3d_suspend.text() == "Suspend":
+            self.scan3d_suspend.setText("Resume")
         else:
-            self.scan3d_suspend.setText("suspend")
+            self.scan3d_suspend.setText("Suspend")
 
-    def update_plot_data_structure(self):
-        # to be done to set the X Y Z component to plot
-        pass
+    def init_plot_data(self, data_structure):
+        nx, xi, xf, ny, yi, yf, nz, zi, zf = data_structure
+        self.Field = np.ndarray([nx, ny, nz, 4])
+        self.X = np.linspace(xi, xf, nx)
+        self.Y = np.linspace(yi, yf, ny)
+        self.Z = np.linspace(zi, zf, nz)
+        self.all_elements = nx*ny*nz
+        self.update_layers()
+        self.update_plot_params()
 
-    def init_plot_data(self,lx,ly):
-        if not self.is_started:
-            self.timer.start(5000)
-            self.is_started = True
-        x = np.arange(lx)
-        y = np.arange(ly)
-        self.X,self.Y = np.meshgrid(x,y)
-        self.Z=np.zeros(self.X.shape)
+    def update_field(self):
+        self._ax.set_zlabel(self.scan3d_fieldcomponent.currentText())
+        self.update_plot_params()
 
+    def update_layers(self):
+        plane = self.scan3d_plane.currentText()
+        self.scan3d_layer.clear()
+        if plane == "xy":
+            self.scan3d_layer.addItems([str(i) for i in self.Z])
+            self._ax.set_xlabel("Y")
+            self._ax.set_ylabel("X")
+        elif plane == "yz":
+            self.scan3d_layer.addItems([str(i) for i in self.X])
+            self._ax.set_xlabel("Z")
+            self._ax.set_ylabel("Y")
+        elif plane == "xz":
+            self.scan3d_layer.addItems([str(i) for i in self.Y])
+            self._ax.set_xlabel("Z")
+            self._ax.set_ylabel("X")
+        self.update_plot_params()
+
+    def update_plot_params(self):
+        plane = self.scan3d_plane.currentText()
+        index_layer = self.scan3d_layer.currentIndex()
+        field_component_index = self.scan3d_fieldcomponent.currentIndex()
+        if plane == "xy":
+            self.X_plot, self.Y_plot = np.meshgrid(self.Y, self.X)
+            self.Z_plot = self.Field[:, :, index_layer, field_component_index]
+        elif plane == "yz":
+            self.X_plot, self.Y_plot = np.meshgrid(self.Z, self.Y)
+            self.Z_plot = self.Field[index_layer, :, :, field_component_index]
+        elif plane == "xz":
+            self.X_plot, self.Y_plot = np.meshgrid(self.Z, self.X)
+            self.Z_plot = self.Field[:, index_layer, :,  field_component_index]
+        print(self.X_plot.shape,self.Y_plot.shape,self.Z_plot.shape)
+        self.update_plot()
 
     def update_plot_data(self,position,field):
-        x,y,z = map(int,position.split(","))
-        bx,by,bz,bn = map(float,field.split(","))
-        self.Z[x,y]= bx
-        print("Field data ",x,y,bx)
+        x, y, z = [float(i) for i in position.split(",")]
+        # give the index in the coordinates
+        #print(x,self.X,np.where(np.isclose(self.X,  x)))
+        i = np.where(np.isclose(self.X,  x))[0][0]
+        j = np.where(np.isclose(self.Y,  y))[0][0]
+        k = np.where(np.isclose(self.Z,  z))[0][0]
+        self.Field[i, j, k] = [float(i) for i in field.split(",")]
+        self.update_plot()
 
     def update_plot(self):
         if self.scan3d_update.isChecked():
@@ -396,36 +429,26 @@ class Scan3dPlotDialog(QDialog, Ui_Dialog):
         if self.plot_object:
             self._ax.collections.remove(self.plot_object)
         if self.scan3d_plottype.currentText() == "surface":
-            self.plot_object = self._ax.plot_surface(self.X, self.Y, self.Z,rstride=1, cstride=1, cmap="viridis", edgecolor="none")
+            self.plot_object = self._ax.plot_surface(self.X_plot, self.Y_plot, self.Z_plot, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
+        #elif self.scan3d_plottype.currentText() == "surface and contour":
+        #    self.plot_object = self._ax.plot_surface(self.X_plot, self.Y_plot, self.Z_plot, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
+        #    cset = self._ax.contour(self.X_plot, self.Y_plot, self.Z_plot, zdir='z', offset=self.Z_plot.min(), cmap=cm.coolwarm)
+        #    cset = self._ax.contour(self.X_plot, self.Y_plot, self.Z_plot, zdir='x', offset=self.X_plot.min(), cmap=cm.coolwarm)
+        #    cset = self._ax.contour(self.X_plot, self.Y_plot, self.Z_plot, zdir='y', offset=self.Y_plot.min(), cmap=cm.coolwarm)
         elif self.scan3d_plottype.currentText() == "wireframe":
-            self.plot_object = self._ax.plot_wireframe(self.X, self.Y, self.Z,rstride=1, cstride=1, cmap="viridis")
+            self.plot_object = self._ax.plot_wireframe(self.X_plot, self.Y_plot, self.Z_plot,rstride=1, cstride=1, cmap="viridis")
+        elif self.scan3d_plottype.currentText() == "quiver":
+            x,y,z = np.meshgrid(self.X, self.Y, self.Z)
+            print(x.shape,y.shape,z.shape,self.Field[:, :, :, 0].shape)
+            self.plot_object = self._ax.quiver(x,y,z,
+                                               self.Field[:, :, :, 0],
+                                               self.Field[:, :, :, 1],
+                                               self.Field[:, :, :, 2],
+                                               length=0.1, normalize=True)
+
         self.canvas.draw()
 
-    def plot_contour(self):
-        cset = self._ax.contour(self.X, self.Y, self.Z, zdir='z', offset=self.Z.min(), cmap=cm.coolwarm)
-        cset = self._ax.contour(self.X, self.Y, self.Z, zdir='x', offset=self.X.min(), cmap=cm.coolwarm)
-        cset = self._ax.contour(self.X, self.Y, self.Z, zdir='y', offset=self.Y.min(), cmap=cm.coolwarm)
 
-    def plot_wire(self):
-        # Data
-        #self.X, self.Y, self.Z = axes3d.get_test_data(0.03)
-        x = np.arange(-30,30)
-        y = np.arange(-30,30)
-        self.X,self.Y = np.meshgrid(x,y)
-        self.Z=np.zeros(self.X.shape)
-        for i in range(len(x)):
-            for j in range(len(y)):
-                self.Z[i,j] = -np.sin(.10 * ((i/10.)**2 + (j/10)**2)) / 10.
-
-        #cset = self._ax.contourf(self.X, self.Y, self.Z, cmap=cm.coolwarm)
-        #self._ax.plot_wireframe(self.X, self.Y, self.Z, rstride=10, cstride=10, cmap="viridis")
-        self._ax.plot_surface(self.X, self.Y, self.Z,
-                              rstride=1, cstride=1, cmap="viridis", edgecolor="none")
-        self.plot_contour()
-        self.canvas.draw()
-
-    def stop_timer(self):
-        self.timer.stop()
 
 # The main class
 class MainWindow(QMainWindow,Ui_MainWindow):
@@ -499,6 +522,10 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.start_scan.clicked.connect(self.scan)
         #self.start_scan.setEnabled(False)
 
+        # datas
+        self.vol_path_3d_data = {}
+        self.scan()
+
         
     def check_state(self, *args, **kwargs):
         sender = self.sender()
@@ -513,20 +540,16 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         sender.setStyleSheet('QLineEdit { background-color: %s }' % color)
 
     def loadScanParam(self):
-        name = QFileDialog.getOpenFileName(self, "Load scan file")
+        name, _ = QFileDialog.getOpenFileName(self, "Load scan file")
+        print(json.loads(open(name)))
+        # need to create vol and path from there. This mean updating the interface and click or execute directly the functions.
+        # the problem is how do we know the coordinates of the point since they can change from one volume to another.
+        # or shall we go from position to path in one click hiding the volume part?
 
     def saveScanParam(self):
-        name = QFileDialog.getSaveFileName(self, 'Save scan file')
-        if name[0] != "":
-            l = np.min([len(self.x), len(self.y), len(self.y1)])
-            if l == 0:
-                dlg = QMessageBox.about(self, "Save Data", "No data to save")
-                return
-            file = open(name[0], 'w')
-            file.write("# t(s)  P(mbar) Pump on (1) 0 (off)")
-            for i in range(l):
-                f.write("%e %e %e\n" % (self.x[i], self.y[i], self.y1[i]))
-            file.close()
+        name, _ = QFileDialog.getSaveFileName(self, 'Save scan file', filter="Scan Files (*.json) ;; All Files (*)", initialFilter='*.json')
+        with open(name, 'w') as file:
+            file.write(json.dumps(self.vol_path_3d_data))
 
     def exitApplication(self):
         """
@@ -718,7 +741,15 @@ class MainWindow(QMainWindow,Ui_MainWindow):
                                            "%s" % (axis_min+origin[c2]),
                                            "%s" % (axis_max+origin[c2]))
             if retcode == 1:
-                self.vol_path_3d_data[vol_id] = {"origin":origin, "ext_axis":ext_axis, "axis_min":axis_min, "axis_max":axis_max, "all_coords":all_coords}
+                self.vol_path_3d_data["volumes"]={
+                    vol_id: {
+                        "origin": origin,
+                        "ext_axis": ext_axis,
+                        "axis_min": axis_min,
+                        "axis_max": axis_max,
+                        "all_coords": all_coords
+                        }
+                }
                 self.create_path.setEnabled(True)
                 self.delete_volume.setEnabled(True)
                 self.volume_nid += 1
@@ -746,14 +777,27 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         scan_y_step = self.scan_y_step.text()
         scan_z_step = self.scan_z_step.text()
 
-
         dlg = askForName()
         dlg.message.setText("Enter path id")
         dlg.lineEditField.setText("path_%d" % self.path_nid)
         if dlg.exec_():
             path_id = dlg.lineEditField.text()
-            retcode,res = self.pyrame.call("init_path@paths",path_id,"space_1",vol_id,scan_x_step,scan_y_step,scan_z_step,path_order,"rr","ppp")
+            path_type = "rr"
+            path_directions = "ppp"
+            retcode,res = self.pyrame.call("init_path@paths",path_id,"space_1",vol_id,scan_x_step,scan_y_step,scan_z_step,path_order,path_type,path_directions)
             if retcode == 1:
+                self.vol_path_3d_data["paths"] = {
+                    path_id: {
+                        "vol_id": vol_id,
+                        "scan_x_step": scan_x_step,
+                        "scan_y_step": scan_y_step,
+                        "scan_z_step": scan_z_step,
+                        "path_order": path_order,
+                        "path_type": path_type,
+                        "path_directions": path_directions
+                        }
+                }
+
                 self.start_scan.setEnabled(True)
                 self.delete_path.setEnabled(True)
                 self.path_nid += 1
@@ -777,6 +821,8 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         p, f = n.split(";")
         self.updatePositionWidget(p)
         self.updateGaussmeterWidget(f)
+
+        # update plot
         self.scan3d_plot.update_plot_data(p,f)
 
     def scan3d_showProgress(self, i):
@@ -785,8 +831,6 @@ class MainWindow(QMainWindow,Ui_MainWindow):
     def scan3d_finished_scan(self):
         self.scan3d_plot.scan3d_stop.setText("Close")
         self.scan3d_plot.scan3d_suspend.setEnabled(False)
-        self.scan3d_plot.stop_timer()
-
 
     def scan3D(self):
         if self.thread_started:
@@ -852,7 +896,8 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.thread.start()
 
         self.scan3d_plot = Scan3dPlotDialog(self.worker)
-        self.scan3d_plot.init_plot_data(30,30)
+        data_structure = [_Nx, _Xi, _Xf, _Ny, _Yi, _Yf, _Nz, _Zi, _Zf]
+        self.scan3d_plot.init_plot_data(data_structure)
         self.scan3d_plot.show()
         self.thread.start()
 
