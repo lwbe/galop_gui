69a73152c7a038e3f70d8608dbd0f86b81b05f6f
Lorenzo Bernardi <lorenzo.bernardi@llr.in2p3.fr>
2021-11-05 17:37:35 +0100

    ajoute une generation de chemin dans paths.py

--- paths.py
+++ paths.py
@@ -20,3 +20,70 @@ def generate_points2(x0,x1,dx,y0,y1,dy,z0,z1,dz):
     l = x.size
     return np.array([(i,j,k) for i,j,k in zip(x.reshape(l,),y.reshape(l,),z.reshape(l,))])
 
+def generate_path(sizes, order):
+    # order is a string containing the order of the scan xyz means scanning x then y then z
+    # sizes contains the sizes for x,y,z
+    map_string_to_index = {
+        'x': 0,
+        'y': 1,
+        'z': 2
+    }
+    # order the sizes
+    l = [sizes[map_string_to_index[d]] for d in order]
+
+    coords = [[],[],[]]
+    for p in range(l[0]*l[1]*l[2]):
+        k = int(p / (l[0]*l[1]))
+        j = int((p-k*l[1]*l[0]) / l[0])
+        i = p - k*l[1]*l[0] - j*l[0]
+
+        ti = i if (int(p / l[0]) % 2 == 0) else (l[0]-1 - i)
+        tj = j if (int(p / (l[0]*l[1])) % 2 == 0) else (l[1]-1 - j)
+
+        coords[0].append(ti)
+        coords[1].append(tj)
+        coords[2].append(k)
+    new_coords = [[],[],[]]
+    for i, j in enumerate([map_string_to_index[d] for d in order]):
+        new_coords[j] = coords[i]
+    return new_coords
+
+def generate_path2(sizes, order):
+    # order is a string containing the order of the scan xyz means scanning x then y then z
+    # sizes contains the sizes for x,y,z
+    map_string_to_index = {
+        'x': 0,
+        'y': 1,
+        'z': 2
+    }
+    # order the sizes
+    l = [sizes[map_string_to_index[d]] for d in order]
+    coords = [[],[],[]]
+    f_asc = False
+    m_asc = False
+    for s in range(l[2]):
+        m_asc = not m_asc
+        for m in range(l[1]):
+            f_asc = not f_asc
+            for f in range(l[0]):
+                coords[0].append(f if f_asc else l[0] - 1 - f)
+                coords[1].append(m if m_asc else l[1] - 1 - m)
+                coords[2].append(s)
+
+    new_coords = [[],[],[]]
+    for i, j in enumerate([map_string_to_index[d] for d in order]):
+        new_coords[j] = coords[i]
+
+    return np.array(new_coords).T
+
+
+
+
+print(generate_path([2,3,4],'xyz'))
+print(generate_path2([2,3,4],'xyz'))
+print(generate_path2([2,3,4],'zxy'))
+a=generate_path2([2,3,4],'xyz')
+b = a*np.array([0.1,0.5,1])#+np.array([3,4,5])
+print(b[1])
+for i in b:
+    print(i)
